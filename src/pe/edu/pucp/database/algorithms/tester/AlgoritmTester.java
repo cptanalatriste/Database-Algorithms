969a8a4b80c846ddc2a4dbfe88d386a2d995c260
Carlos Gavidia <cgavidiac@gmail.com>
2012-06-17 11:35:33 -0500

    Basic project configuration

---
+++ src/pe/edu/pucp/database/algorithms/tester/AlgoritmTester.java
@@ -0,0 +1,16 @@
+package pe.edu.pucp.database.algorithms.tester;
+
+/**
+ * @author cgavidia
+ *
+ */
+public class AlgoritmTester {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		System.out.println("Hola mundo!");
+	}
+
+}
